0226e3b3a9174dff9d91e0d30a76b1dce655d3c9
MarcosRC <marcosrillocabanne@gmail.com>
2024-11-29 10:43:14 -0300

    listo WhatWeDo

--- app/components/WhatWeDo.jsx
+++ app/components/WhatWeDo.jsx
@@ -1,43 +1,44 @@
 import {
-  LaptopMac as LaptopMacIcon,
+  DevicesOtherOutlined as DevicesOtherOutlinedIcon,
   People as PeopleIcon,
   Web as WebIcon,
 } from "@mui/icons-material";
+import { montserrat } from "../ui/fonts";
 
 export default function WhatWeDo({ openModal }) {
   return (
     <section
       id="what-we-do"
-      className="bg-white flex flex-col justify-center items-center min-h-screen py-16"
+      className="bg-white flex flex-col justify-center items-center min-h-screen py-24"
     >
-      <div className="container mx-auto text-center">
-        <h2 className="text-4xl md:text-5xl font-bold text-gray-800 mb-6">
-          ¿Qué hacemos?
+      <div className="container mx-auto w-10/12 md:w-9/12 lg:w-10/12 text-center">
+        <h2 className={`${montserrat.className} text-5xl font-bold text-gray-700 pb-10`}>
+          ¿QUE HACEMOS?
         </h2>
-        <strong className="text-lg md:text-xl text-gray-600 mb-12">
+        <strong className="text-md leading-8 md:text-xl font-bold text-gray-500 my-12">
           Trabajamos formando equipo con las empresas, instituciones y
           organizadores profesionales que producen eventos, brindándoles
           herramientas tecnológicas para optimizar sus tareas.{" "}
         </strong>
-        <div className="flex flex-col md:flex-row justify-center items-center gap-8 border-2 border-conti-blue">
-          <div className="bg-white p-8 py-16 w-auto flex flex-col items-center lg:hover:shadow-xl">
-            <LaptopMacIcon className="text-conti-blue" sx={{ fontSize: 80 }} />
-            <h3 className="text-2xl font-bold text-gray-800 mb-4">
+        <div className="flex flex-col lg:flex-row justify-center items-center gap-8 mt-10">
+          <div className="bg-white p-8 py-8 w-auto flex flex-col items-center rounded-xl lg:hover:shadow-2xl lg:hover:shadow-gray-400 transition-all duration-500 ease-in-out">
+            <DevicesOtherOutlinedIcon className="text-conti-blue" sx={{ fontSize: 70 }} />
+            <h3 className="text-2xl font-bold text-gray-800 my-4">
               Eventos Virtuales
             </h3>
-            <p className="text-gray-600 text-center font-normal border-2 border-conti-blue">
+            <p className="text-gray-500 text-center font-normal leading-8">
               Nuestra plataforma integra páginas web, formularios de registro
               con pasarela de pagos, sedes 3D, salas, streaming de conferencias,
               control de ingresos, certificación de asistencia, encuestas y
               métricas finales, todo funcionando en un mismo entorno.{" "}
             </p>
           </div>
-          <div className="bg-white p-8 flex flex-col items-center">
-            <PeopleIcon className="text-conti-blue" sx={{ fontSize: 80 }} />
-            <h3 className="text-2xl font-bold text-gray-800 mb-4">
+          <div className="bg-white p-8 flex flex-col items-center rounded-xl lg:hover:shadow-2xl lg:hover:shadow-gray-400 transition-all duration-500 ease-in-out">
+            <PeopleIcon className="text-conti-blue" sx={{ fontSize: 70 }} />
+            <h3 className="text-2xl font-bold text-gray-800 my-4">
               Eventos Presenciales
             </h3>
-            <p className="text-gray-600 text-center font-normal">
+            <p className="text-gray-500 text-center font-normal leading-8">
               Servicio de acreditaciones on site vinculado a generación previa
               de códigos QR, campañas de confirmación y recordatorios, lectura
               con dispositivos móviles, autoacreditaciones y controles de
@@ -45,12 +46,12 @@ export default function WhatWeDo({ openModal }) {
               armoniosamente para lograr un servicio ágil y eficiente.
             </p>
           </div>
-          <div className="bg-white p-8 flex flex-col items-center">
-            <WebIcon className="text-conti-blue" sx={{ fontSize: 80 }} />
-            <h3 className="text-2xl font-bold text-gray-800 mb-4">
+          <div className="bg-white p-8 flex flex-col items-center rounded-xl lg:hover:shadow-2xl lg:hover:shadow-gray-400 transition-all duration-500 ease-in-out">
+            <WebIcon className="text-conti-blue" sx={{ fontSize: 70 }} />
+            <h3 className="text-2xl font-bold text-gray-800 my-4">
               Eventos Híbridos
             </h3>
-            <p className="text-gray-600 text-center font-normal">
+            <p className="text-gray-500 text-center font-normal leading-8">
               El mix entre la virtualidad y la presencialidad genera eventos que
               combinan lo mejor de los 2 mundos. Nos encargamos de integrarlos y
               darles soluciones en todas las instancias de la vida de un
@@ -60,9 +61,9 @@ export default function WhatWeDo({ openModal }) {
         </div>
         <button
           onClick={openModal}
-          className="mt-12 px-6 py-3 border border-transparent rounded-3xl text-sm font-medium text-white bg-conti-blue focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          className="mt-16 px-6 py-3 border-4 lg:px-12 lg:py-6 border-conti-blue rounded-full text-sm md:text-lg font-bold text-white bg-conti-blue hover:bg-white hover:text-conti-blue transition-all duration-300 ease-in-out"
         >
-          Agende una reunión
+          AGENDE UNA REUNION
         </button>
       </div>
     </section>
